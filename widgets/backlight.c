62d57a41daf9691ab046900bb7d84e53d079bf66
oda404 <xdxalexandru404@gmail.com>
2022-07-22 22:58:36 +0300

    backlight: Formatting + timeout for backlight change.

--- widgets/backlight.c
+++ widgets/backlight.c
@@ -5,24 +5,25 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 static const char *g_backlight_get = "xbacklight -get";
-static const char *g_backlight_raise =  "xbacklight -inc 5";
+static const char *g_backlight_raise = "xbacklight -inc 5";
 static const char *g_backlight_lower = "xbacklight -dec 5";
 
-#define WIDGET_ABORT    \
-{                       \
-	w->active = false;  \
-	return false;       \
-}
+#define WIDGET_ABORT       \
+	{                      \
+		w->active = false; \
+		return false;      \
+	}
 
 bool widget_backlight_init(struct S_Widget *w)
 {
-    if(!(w->text = malloc(5)))
+	if (!(w->text = malloc(5)))
 		WIDGET_ABORT;
-	
+
 	/* Try to execute the volume_get_cmd to see if it's valid. If not abort. */
-	if(!exec_cmd(g_backlight_get, w->text, 4))
+	if (exec_cmd(g_backlight_get, w->text, 4) != 0)
 		WIDGET_ABORT;
 
 	size_t buflen = strlen(w->text);
@@ -31,24 +32,24 @@ bool widget_backlight_init(struct S_Widget *w)
 
 	w->active = true;
 	w->should_redraw = true;
-	
+
 	return true;
 }
 
 void widget_backlight_event(const Arg *arg)
 {
-    if(!arg || !arg->v)
+	if (!arg || !arg->v)
 		return;
-        
+
 	const char *cmd = ((const char **)arg->v)[0];
 	Widget *w = ((Widget **)arg->v)[1];
 
-    if(!cmd || !w)
-        return;
+	if (!cmd || !w)
+		return;
 
-    const char *xbacklight_call = NULL;
+	const char *xbacklight_call = NULL;
 
-	switch(*cmd)
+	switch (*cmd)
 	{
 	case '+':
 		xbacklight_call = g_backlight_raise;
@@ -60,11 +61,20 @@ void widget_backlight_event(const Arg *arg)
 		return;
 	}
 
-	if(!(exec_cmd(xbacklight_call, NULL, 0) && exec_cmd(g_backlight_get, w->text, 4)))
+	if (exec_cmd(xbacklight_call, NULL, 0) != 0)
 	{
 		w->active = false;
 		return;
 	}
+
+	/* My backlight takes a while to update and be accessible again, so we stall a bit */
+	int tries = 10;
+	while (exec_cmd(g_backlight_get, w->text, 4) != 0 && (tries-- > 0))
+		usleep(10000);
+
+	if (tries == 0)
+		/* might as well assume the last call also failed :( */
+		return;
 
 	size_t buflen = strlen(w->text);
 	w->text[buflen] = '%';
@@ -75,6 +85,6 @@ void widget_backlight_event(const Arg *arg)
 
 void widget_backlight_destroy(struct S_Widget *w)
 {
-    if(w->text)
+	if (w->text)
 		free(w->text);
 }
