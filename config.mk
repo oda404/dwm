7a302566d87be8f2390dea7275797493977b1bbb
oda404 <xdxalexandru404@gmail.com>
2022-07-22 23:08:24 +0300

    config.mk: Use -std=c2x and -march=${MARCH}.
    MARCH being your cpu architecture, by default znver1 (zen 1).

--- config.mk
+++ config.mk
@@ -25,10 +25,12 @@ FREETYPEINC = /usr/include/freetype2
 INCS = -I${X11INC} -I${FREETYPEINC}
 LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
+MARCH = znver1
+
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O3 ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c2x -pedantic -Wall -Wno-deprecated-declarations -O3 -march=${MARCH} ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
