e61217310203c14c09ebf5627869f04f3e0d5362
oda404 <xdxalexandru404@gmail.com>
2023-03-06 16:49:25 +0200

    network: Use posix API to get network info.

--- widgets/network.c
+++ widgets/network.c
@@ -4,102 +4,67 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <net/if.h>
 
 #define NETWORK_NAME_SIZE 32
 #define NETWORK_IPV4_SIZE 16
-#define NETWORK_BUF_SIZE (NETWORK_NAME_SIZE + NETWORK_IPV4_SIZE + 8)
-
-static const char *network_get_info_cmd = "ip route get 8.8.8.8";
 
 int widget_network_init(struct S_Widget *w)
 {
 	w->active = true;
 	widget_network_update(w);
 
-	return true;
+	return 0;
 }
 
 bool widget_network_update(struct S_Widget *w)
 {
-	char info[NETWORK_BUF_SIZE];
+	struct ifaddrs *ifaddr = NULL;
+	struct ifaddrs *avail_ifa = NULL;
 
-	if (exec_cmd(network_get_info_cmd, info, NETWORK_BUF_SIZE) != 0)
+	char host[NI_MAXHOST] = {0};
+
+	if (getifaddrs(&ifaddr) < 0)
 	{
-		strcpy(w->text, "disconnected");
+		strncpy(w->text, "No interfaces!", WIDGET_TEXT_MAXLEN);
 		return true;
 	}
 
-	char *interfaceloc = strstr(info, "dev ");
-	if (!interfaceloc)
+	for (struct ifaddrs *ifa = ifaddr; ifa; ifa = ifa->ifa_next)
 	{
-		strcpy(w->text, "disconnected");
-		return true;
+		/* Not connected to a host. */
+		if (!ifa->ifa_addr)
+			continue;
+
+		/* We only care about non-loopback, IPv4 interfaces. */
+		if (ifa->ifa_addr->sa_family != AF_INET || ifa->ifa_flags & IFF_LOOPBACK)
+			continue;
+
+		/* Just use the first one we hit :) */
+		avail_ifa = ifa;
 	}
 
-	interfaceloc += 4;
-
-	char ip[NETWORK_IPV4_SIZE] = {0};
-
+	if (!avail_ifa)
 	{
-		char *iploc = strstr(info, "src ");
-		if (iploc)
-		{
-			iploc += 4;
-			for (size_t i = 0; i < NETWORK_IPV4_SIZE; ++i)
-			{
-				if (iploc[i] == ' ')
-				{
-					ip[i] = '\0';
-					break;
-				}
-
-				ip[i] = iploc[i];
-			}
-		}
-		else
-		{
-			strncpy(ip, "???", NETWORK_IPV4_SIZE);
-		}
+		strncpy(w->text, "All interfaces down!", WIDGET_TEXT_MAXLEN);
+		goto quit;
 	}
 
-	/* Wireless interfaces start with a 'w'. */
-	const bool wired = (interfaceloc[0] != 'w');
+	size_t structsize = sizeof(struct sockaddr_in);
+	int st = getnameinfo(avail_ifa->ifa_addr, structsize, host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
 
-	if (wired)
+	if (st != 0)
 	{
-		w->icon = " ";
-		strncpy(w->text, ip, NETWORK_BUF_SIZE);
-	}
-	else
-	{
-		w->icon = " ";
-
-		char interface[NETWORK_NAME_SIZE] = {0};
-		for (size_t i = 0; i < NETWORK_NAME_SIZE; ++i)
-		{
-			if (interfaceloc[i] == ' ')
-			{
-				interface[i] = '\0';
-				break;
-			}
-
-			interface[i] = interfaceloc[i];
-		}
-
-		char network_name_cmd[128] = {0};
-		snprintf(
-			network_name_cmd,
-			128,
-			"iwctl station %s show | grep 'Connected network' | sed 's/Connected network//' | sed -r 's/\\s+//g'",
-			interface);
-
-		char name[NETWORK_NAME_SIZE];
-		if (exec_cmd(network_name_cmd, name, NETWORK_NAME_SIZE) != 0)
-			strncpy(name, "???", NETWORK_NAME_SIZE);
-
-		snprintf(w->text, NETWORK_BUF_SIZE, "%s (%s)", name, ip);
+		strncpy(w->text, "Internal error!", WIDGET_TEXT_MAXLEN);
+		goto quit;
 	}
 
+	snprintf(w->text, WIDGET_TEXT_MAXLEN, "%s (%s)", avail_ifa->ifa_name, host);
+
+quit:
+	freeifaddrs(ifaddr);
 	return true;
 }
 
