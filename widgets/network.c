06c1b33c308e4135f469b91b801e0cadfe00e3c5
oda404 <xdxalexandru404@gmail.com>
2023-12-10 02:21:26 +0200

    network: Add rx/tx speed.

--- widgets/network.c
+++ widgets/network.c
@@ -7,9 +7,42 @@
 #include <netdb.h>
 #include <net/if.h>
 #include <linux/if_ppp.h>
+#include <linux/if_link.h>
+#include <dwm/types.h>
 
 #define NETWORK_NAME_SIZE 32
 #define NETWORK_IPV4_SIZE 16
+
+float bytes_to_human_readable(u32 bytes, char unit[4])
+{
+	float fbytes = bytes;
+	u8 i = 0;
+	while (fbytes >= 1024)
+	{
+		++i;
+		fbytes /= 1024.f;
+	}
+
+	if (unit)
+	{
+		switch (i)
+		{
+		case 0:
+			strcpy(unit, "B");
+			break;
+		case 1:
+			strcpy(unit, "KiB");
+			break;
+		case 2:
+			strcpy(unit, "MiB");
+			break;
+		case 3:
+			strcpy(unit, "GiB");
+			break;
+		}
+	}
+	return fbytes;
+}
 
 int widget_network_init(struct S_Widget *w)
 {
@@ -18,6 +51,8 @@ int widget_network_init(struct S_Widget *w)
 
 	return 0;
 }
+
+static u32 prev_rx, prev_tx;
 
 bool widget_network_update(struct S_Widget *w)
 {
@@ -32,6 +67,10 @@ bool widget_network_update(struct S_Widget *w)
 		return true;
 	}
 
+	static u32 tx = 0, rx = 0;
+	prev_rx = rx;
+	prev_tx = tx;
+
 	for (struct ifaddrs *ifa = ifaddr; ifa; ifa = ifa->ifa_next)
 	{
 		/* Not connected to a host. */
@@ -39,11 +78,25 @@ bool widget_network_update(struct S_Widget *w)
 			continue;
 
 		/* We only care about non-loopback, IPv4 interfaces. */
-		if (ifa->ifa_addr->sa_family != AF_INET || ifa->ifa_flags & IFF_LOOPBACK)
-			continue;
+		switch (ifa->ifa_addr->sa_family)
+		{
+		case AF_PACKET:
+		{
+			if (!ifa->ifa_data)
+				break;
 
-		/* Just use the first one we hit :) */
-		avail_ifa = ifa;
+			struct rtnl_link_stats *stats = ifa->ifa_data;
+			tx = stats->tx_bytes;
+			rx = stats->rx_bytes;
+			break;
+		}
+		case AF_INET:
+			avail_ifa = ifa;
+			break;
+
+		default:
+			break;
+		}
 	}
 
 	if (!avail_ifa)
@@ -61,10 +114,13 @@ bool widget_network_update(struct S_Widget *w)
 		goto quit;
 	}
 
-	// struct ppp_stats ppp;
-	// struct ifreq req;
+	char rx_units[4];
+	char tx_units[4];
 
-	snprintf(w->text, WIDGET_TEXT_MAXLEN, "%s (%s)", avail_ifa->ifa_name, host);
+	float delta_rx = bytes_to_human_readable(rx - prev_rx, rx_units);
+	float delta_tx = bytes_to_human_readable(tx - prev_tx, tx_units);
+
+	snprintf(w->text, WIDGET_TEXT_MAXLEN, "%s (%s) | %05.1f %3s  | %05.1f %3s  |", avail_ifa->ifa_name, host, delta_rx, rx_units, delta_tx, tx_units);
 
 quit:
 	freeifaddrs(ifaddr);
